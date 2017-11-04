356b1e27a091c2aaf4410169abe39fc419686c53
Anthonylee <anthonylee@umass.edu>
2017-11-04 16:59:16 -0400

    Selected class

---
+++ PEEKlass/src/Selected.java
@@ -0,0 +1,28 @@
+public class Selected extends ClassList(){
+	
+	public void add(Course c){
+		if (courselist.containsKey(c.getCourseNum())){
+			return;
+		} else{
+			courselist.put(c.getCourseNum(), c);
+		}
+	}
+	
+	public void delete(Course c){
+		if (courselist.containsKey(c.getCourseNum())){
+			return;
+		} else{
+			courselist.remove(c.getCourseNum());
+		} 
+	}
+
+	public void moveToMT(Course c, MustTake a){
+		//if Course c is in courselist and not in MustTake
+		if (courselist.containsKey(c.getCourseNum()) && a.isOverlap(c)){
+			this.delete(c);
+			a.add(c);
+		} else {
+			return ; //else?
+		}
+	}
+}
