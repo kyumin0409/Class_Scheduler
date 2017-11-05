706b1411bbae2cbd93adca345296ac88655c1ce5
cchaechae <seo23c@mtholyoke.edu>
2017-11-04 22:29:35 -0400

    Filter methods added

--- PEEKlass/src/COMPSCI.java
+++ PEEKlass/src/COMPSCI.java
@@ -1,24 +1,19 @@
 
 public class COMPSCI extends Major {
 
-<<<<<<< HEAD
 	private AllCourses ac = new AllCourses();
 
 	public COMPSCI(){
 		
-	coreList.add(ac.getCS121());
-	coreList.add(ac.getCS187());
-	coreList.add(ac.getCS311());
-
-	electiveList.add(ac.getCS326());
-	electiveList.add(ac.getCS345());
-	electiveList.add(ac.getCS377());
-	
-=======
-	//make a 2D array of requirements
-	public COMPSCI(){
-		
 		majorName = "COMPSCI";
->>>>>>> 2b295305cbb76ad8d51819716b314b4e6df2b362
+		
+		coreList.add(ac.getCS121());
+		coreList.add(ac.getCS187());
+		coreList.add(ac.getCS311());
+
+		electiveList.add(ac.getCS326());
+		electiveList.add(ac.getCS345());
+		electiveList.add(ac.getCS377());
 	}
+
 }
