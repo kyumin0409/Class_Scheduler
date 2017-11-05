29dcc5d8f3aebe534a38a7e0a0d930b6369e0a0a
Ye Won <hyw1397@gmail.com>
2017-11-04 21:57:46 -0400

    update

--- PEEKlass/src/Math.java
+++ PEEKlass/src/Math.java
@@ -1,4 +1,16 @@
 
-public class Math extends Major{
+public class MATH extends Major{
+	
+	private AllCourses ac = new AllCourses();
 
+	public MATH(){
+		
+	coreList.add(ac.getMath131());
+	coreList.add(ac.getMath132());
+	coreList.add(ac.getMath233());
+
+	electiveList.add(ac.getMath235());
+	electiveList.add(ac.getMath331());
+	
+	}
 }
