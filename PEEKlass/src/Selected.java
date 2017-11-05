5a7785d39c7407fd3e271b6c12649ddca612518c
Anthonylee <anthonylee@umass.edu>
2017-11-04 21:28:55 -0400

    Selected revised

--- PEEKlass/src/Selected.java
+++ PEEKlass/src/Selected.java
@@ -1,34 +1,43 @@
+import java.time.LocalTime;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Selected extends ClassList{
 	
-	public void add(Course c){
+	public boolean add(Course c){
 		if (courselist.containsKey(c.getCourseNum())){
-			return;
+			return false;
 		} else{
 			courselist.put(c.getCourseNum(), c);
+			return true;
 		}
 	}
 	
-	public void delete(Course c){
+	public boolean delete(Course c){
 		if (courselist.containsKey(c.getCourseNum())){
-			return;
+			return false;
 		} else{
 			courselist.remove(c.getCourseNum());
+			return true;
 		} 
-	}
-
-	public void moveToMT(Course c, MustTake a){
-		//if Course c is in courselist and not in MustTake
-		if (courselist.containsKey(c.getCourseNum()) && a.isOverlap(c)){
-			this.delete(c);
-			a.add(c);
-		} else {
-			return ; //else?
-		}
 	}
 
 	@Override
 	public boolean isOverlap(Course c) {
-		// TODO Auto-generated method stub
+		Iterator it = courselist.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry pair = (Map.Entry)it.next();
+			LocalTime currStart = ((Course) pair.getValue()).getStartTime();
+			LocalTime currEnd = ((Course) pair.getValue()).getEndTime();
+			if(currStart.compareTo(c.getStartTime())>0 && currStart.compareTo(c.getEndTime())<0){
+				return true;
+			}else if(c.getStartTime().compareTo(currStart)>0 && c.getStartTime().compareTo(currEnd)<0){
+				return true;
+			}
+			else{
+				continue;
+			}
+		}
 		return false;
 	}
 }
