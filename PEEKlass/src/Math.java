41fd2eaff58618eda5bde8ecf4624f09fe438647
Ye Won <hyw1397@gmail.com>
2017-11-04 22:07:26 -0400

    c

--- PEEKlass/src/Math.java
+++ PEEKlass/src/Math.java
@@ -1,25 +1,22 @@
 
-<<<<<<< HEAD
 public class MATH extends Major{
 	
 	private AllCourses ac = new AllCourses();
-=======
-public class Math extends Major{
 	
-	public Math(){
-		
-		majorName = "Math";
-	}
->>>>>>> 2b295305cbb76ad8d51819716b314b4e6df2b362
+	public class Math extends Major{
+	
+		public Math(){
+			majorName = "Math";
+		}
 
-	public MATH(){
+		public MATH(){
 		
-	coreList.add(ac.getMath131());
-	coreList.add(ac.getMath132());
-	coreList.add(ac.getMath233());
-
-	electiveList.add(ac.getMath235());
-	electiveList.add(ac.getMath331());
+			coreList.add(ac.getMath131());
+			coreList.add(ac.getMath132());
+			coreList.add(ac.getMath233());
+			
+			electiveList.add(ac.getMath235());
+			electiveList.add(ac.getMath331());
 	
 	}
 }
